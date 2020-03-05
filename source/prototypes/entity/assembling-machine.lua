134750f8862c794bb60d1710a6b47cd93a4c1421
Asher Merrill <asher.s.merrill@gmail.com>
2020-03-04 19:04:17 -0500

    prototypes: ent: polution and balancing.

--- source/prototypes/entity/assembling-machine.lua
+++ source/prototypes/entity/assembling-machine.lua
@@ -1,15 +1,15 @@
 data.raw["assembling-machine"]["assembling-machine-1"].crafting_speed = 0.75
-data.raw["assembling-machine"]["assembling-machine-1"].crafting_categories = {"crafting", "advanced-crafting", "crafting-with-fluid"}
+data.raw["assembling-machine"]["assembling-machine-1"].crafting_categories = {"basic-crafting", "crafting", "advanced-crafting", "crafting-with-fluid"}
 data.raw["assembling-machine"]["assembling-machine-1"].energy_usage = "100kW"
 data.raw["assembling-machine"]["assembling-machine-1"].ingredient_count = 6
 
 data.raw["assembling-machine"]["assembling-machine-2"].crafting_speed = 1.5
-data.raw["assembling-machine"]["assembling-machine-2"].crafting_categories = {"crafting", "advanced-crafting", "crafting-with-fluid"}
+data.raw["assembling-machine"]["assembling-machine-2"].crafting_categories = {"basic-crafting", "crafting", "advanced-crafting", "crafting-with-fluid"}
 data.raw["assembling-machine"]["assembling-machine-2"].energy_usage = "300kW"
 data.raw["assembling-machine"]["assembling-machine-2"].ingredient_count = 6
 
 data.raw["assembling-machine"]["assembling-machine-3"].crafting_speed = 3
-data.raw["assembling-machine"]["assembling-machine-3"].crafting_categories = {"crafting", "advanced-crafting", "crafting-with-fluid"}
+data.raw["assembling-machine"]["assembling-machine-3"].crafting_categories = {"basic-crafting", "crafting", "advanced-crafting", "crafting-with-fluid"}
 data.raw["assembling-machine"]["assembling-machine-3"].energy_usage = "900kW"
 data.raw["assembling-machine"]["assembling-machine-3"].ingredient_count = 6
 
